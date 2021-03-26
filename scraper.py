9eecb38c29e8f38f6e43538e60eba6e836bd61bc
mikewillems <mikewillems@user.noreply.github.com>
2021-03-26 16:43:39 -0700

    split off data retrieval vs scraping logic, fixed bug when job not found / valid

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
+import data_io
 import pandas as pd 
 import requests
 from typing import List 
-
-from debug_tools import dprint
+from debug_tools import *
 
 """
     Function that takes as input a job title and returns a dataframe
@@ -10,22 +10,24 @@ from debug_tools import dprint
 
 """
 
-def get_salary_table_for_job_title(job_name: str) -> pd.DataFrame:
+def scrape_salary_table_for_job_title(job_name: str) -> pd.DataFrame:
     if(job_name):
         query = job_name.replace(" ", "-")
     else:
         return False
     job_url = "https://ziprecruiter.com/Salaries/What-Is-the-Average-" + query + "-Salary-by-State"
     job_response = requests.get(job_url, timeout=10)
-    dprint(job_url)
-    
+    dprint(job_response)
     if "ind=null" in job_response.url or "Moved Permanently" in job_response.url:
         return False
     elif job_response.status_code == 301:
         return False 
     else:
         job_text: List[pd.DataFrame] = pd.read_html(job_response.text)
-        job_table: pd.DataFrame = pd.concat(job_text)
+        if(len(job_text) > 0):
+            job_table: pd.DataFrame = pd.concat(job_text)
+        else:
+            return False
         return job_table
 
 """
@@ -34,13 +36,9 @@ def get_salary_table_for_job_title(job_name: str) -> pd.DataFrame:
     * TODO: cache this somewhere
 """
 
-def get_ppp_table() -> pd.DataFrame:
+def scrape_ppp_table() -> pd.DataFrame:
     ppp_url = "https://www.patriotsoftware.com/blog/accounting/average-cost-living-by-state/"
     ppp_response = requests.get(ppp_url, timeout=10)
-    dprint(ppp_response)
     ppp_text: List[pd.DataFrame] = pd.read_html(ppp_response.text, header=0)
     ppp_table: pd.DataFrame = pd.concat(ppp_text)
-    dprint(ppp_table.head())
     return ppp_table
-
-
