2619ac4eb8f10c2174a46fb4139c92f216def031
mikewillems <mikewillems@users.noreply.github.com>
2021-03-26 16:43:39 -0700

    split off data retrieval vs scraping logic, fixed bug when job not found / valid

--- dash_app.py
+++ dash_app.py
@@ -1,7 +1,5 @@
 from debug_tools import *
-import scraper
-import data_io
-import clean_data
+import retrieve_data
 
 import pandas as pd
 import plotly.express as px  # (version 4.7.0)
@@ -15,45 +13,10 @@ import urllib
 from urllib.error import HTTPError
 app = dash.Dash(__name__)
 
-# ---------- Import and clean data (importing csv into pandas)
-# try:
-#     df = pd.read_csv("https://raw.githubusercontent.com/Mike-Xie/US_Salaries/main/salaries.csv")
-# except(urllib.error.HTTPError):
-#     pass
-# df.reset_index(inplace=True)
-# dprint(df[:5])
-
-def get_salary_table_for_job_title(job_title: str):
-    salary_data = scraper.get_salary_table_for_job_title(job_title)
-    if (type(salary_data) == pd.DataFrame):
-        ppp = scraper.get_ppp_table()
-        return clean_data.engineer_features(salary_data, ppp)
-    else:
-        return None
-
-# salary_graph = False
-
 # ------------------------------------------------------------------------------
 # App layout
 app.layout = html.Div([
-
     html.H1("Salary Adjusted by Purchasing Price Parity", style={'text-align': 'center'}),
-
-    # TODO: make this a search box 
-    # (https://www.youtube.com/watch?v=VZ6IdRMc0RI&ab_channel=CharmingData) 
-    # not dropdown, if we want dropdown for something else, use this: 
-    # https://dash.plotly.com/pattern-matching-callbacks
-
-    # dcc.Dropdown(id="select_job_title",
-    #              options=[
-    #               #   {"label": "2015", "value": 2015},
-    #               #   {"label": "2016", "value": 2016},
-    #               #   {"label": "2017", "value": 2017},
-    #                  {"label": "Data Science", "value": "Data_Science"}],
-    #              multi=False,
-    #              value="Data_Science",
-    #              style={'width': "40%"}
-    #              ),
     html.Div([dcc.Input(
         id="job_title_search",
         type="search",
@@ -63,15 +26,12 @@ app.layout = html.Div([
         )],
         style={'text-align':'center'},
     ),
-
     html.H3(id='job_title_label', children=[], style={'text-align': 'center'}),
     html.Br(),
-
-    # dcc.Graph(id='plotly_display_element', figure={}) if salary_graph else html.H2("Enter a job title!", style={'text-align': 'center'}),
     dcc.Graph(id='plotly_display_element', figure={})
-
 ])
 
+search_box_input_cache = ""
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
@@ -80,7 +40,6 @@ app.layout = html.Div([
         Output(component_id='job_title_label', component_property='children'),
         Output(component_id='plotly_display_element', component_property='figure'),
      ],
-    # [Input(component_id='select_job_title', component_property='value'),
     [
         Input(component_id='job_title_search', component_property='value')
     ],
@@ -88,14 +47,12 @@ app.layout = html.Div([
 def update_graph(search_box_input):
     dprint(search_box_input)
     dprint(type(search_box_input))
-
+    df = retrieve_data.get_salary_table_for_job_title(search_box_input)
     container = "Showing {} salaries.".format(search_box_input) if search_box_input else "Showing median over all occupations. Enter a job title for specifics."
-
-    df = get_salary_table_for_job_title(search_box_input)
-
+    if (search_box_input and type(df) != pd.DataFrame):
+        container = "No data for {} salaries, showing median salary over all occupations.".format(search_box_input)
     if (type(df) == pd.DataFrame):
         dprint (df.head())
-        # Plotly Express
         fig = px.choropleth(
             data_frame=df,
             locationmode='USA-states',
@@ -109,7 +66,7 @@ def update_graph(search_box_input):
         )
     else:
         fig = px.choropleth(
-            data_frame=get_salary_table_for_job_title('programmer'),
+            data_frame=retrieve_data.get_salary_table_for_job_title('programmer'),
             locationmode='USA-states',
             locations='State Initial',
             scope="usa",
@@ -119,9 +76,9 @@ def update_graph(search_box_input):
             labels={'Adjusted Annual Salary': 'Annual Salary Adjusted for Purchasing Power'},
             template='plotly_dark'
         )
+        
 
     return container, fig, 
-
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
