1b13fe051d4473e13d10bd764138564dc36e8b38
mikewillems <mikewillems@user.noreply.github.com>
2021-03-26 20:45:04 -0700

    fully implemented salary data and bad query caching, implemented fallback in most recent queries

--- data_io.py
+++ data_io.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os.path
+import csv
+from debug_tools import *
 
 # fully qualified file_name (i.e. with extension)
 def write_df(df: pd.DataFrame, file_name: str) -> None:
@@ -9,6 +11,20 @@ def read_df(file_name: str) -> pd.DataFrame:
     return pd.read_csv(file_name)
 
 def file_exists(file_name: str) -> bool:
-    return os.path.isfile(ppp_name)
+    return os.path.isfile(file_name)
 
 # TODO: connect to AWS as credentialed app
+
+def read_csv_to_list(file_name: str) -> list:
+    lines = []
+    with open(file_name, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            lines+=row
+    if(len(lines)>0):
+        return lines
+
+def write_list_to_csv(data: list, file_name: str):
+    data_str = ','.join(data)
+    with open(file_name, 'w') as csvfile:
+        csvfile.write(data_str)
