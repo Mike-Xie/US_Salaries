b32ecb9ac9a3d003cb09dbf2ee75d3fb5fa307ee
mikewillems <mikewillems@user.noreply.github.com>
2021-04-08 13:50:49 -0700

    fixed bug in eng_f - removed refs to api_calls vars, refactored to use squeeze

--- engineer_features.py
+++ engineer_features.py
@@ -23,12 +23,13 @@ def engineer_features(base_salary_table: pd.DataFrame, ppp_table: pd.DataFrame, 
     # get income tax per state from the salary table
     # salary cola dataframe
     all_tax_df = salary_cola_table.merge(salary_cola_table[['Annual Salary', 'State Initial']].apply(
-        lambda state_row: pd.Series(get_income_tax(state_row['State Initial'], state_row['Annual Salary']).to_numpy()[0], index=get_income_tax(state_row['State Initial'],state_row['Annual Salary']).columns), axis=1, result_type='expand'
-    ))
+        lambda state_row: get_income_tax(state_row['State Initial'], state_row['Annual Salary']).squeeze(), axis=1)
+        # lambda state_row: pd.Series(get_income_tax(state_row['State Initial'], state_row['Annual Salary']).to_numpy()[0], index=get_income_tax(state_row['State Initial'],state_row['Annual Salary']).columns), axis=1, result_type='expand'
+    )
 
     # create new features
     all_tax_df['Total Annual Tax'] = all_tax_df['annual.fica.amount'] + all_tax_df['annual.federal.amount'] + all_tax_df['annual.state.amount']
-    all_tax_df['Post Tax Annual Salary'] = all_tax_df['Total Annual Tax'].apply(lambda total_tax: yearly_gross_income - total_tax)
+    all_tax_df['Post Tax Annual Salary'] = all_tax_df['Annual Salary'] - all_tax_df['Total Annual Tax']
 
     # dprint(all_tax_all_tax_df.head())
     return all_tax_df
