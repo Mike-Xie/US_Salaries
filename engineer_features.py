9e22017b953c7b9733fb6d7261f3f82fcd53bb9c
mikewillems <mikewillems@user.noreply.github.com>
2021-04-12 12:49:36 -0700

    fixed bugs regarding labels (eg 'State Initial') - now just need to get clean_data applied properly to remove dollar signs

--- engineer_features.py
+++ engineer_features.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import us_states_and_territories as states
-from clean_data import replace_dollar_with_float
 from debug_tools import dprint
 from math import floor
 
@@ -9,14 +8,16 @@ from math import floor
 # time they are passed here. This needs to combine ppp, tax table, and base salary table with the right column names
 def engineer_features(base_salary_table: pd.DataFrame, ppp_table: pd.DataFrame, tax_table: pd.DataFrame) -> pd.DataFrame:
 
-    # salary_table_with_ppp = base_salary_table.merge(ppp_table, on='State')
+    salary_table_with_ppp = base_salary_table.merge(ppp_table, on='State')
 
     # need State initials because taxee_api and dash both use them 
     salary_table_with_ppp['State Initial'] = salary_table_with_ppp['State'].map(states.states_only)
+    dprint(salary_table_with_ppp.head())
+    dprint(tax_table.head())
     sal_tax_ppp = salary_table_with_ppp.merge(tax_table, on='State Initial')
 
     # create new features
-    # sal_tax_ppp['Total Annual Tax'] = sal_tax_ppp['annual.fica.amount'] + sal_tax_ppp['annual.federal.amount'] + sal_tax_ppp['annual.state.amount']
+    sal_tax_ppp['Total Annual Tax'] = sal_tax_ppp['annual.fica.amount'] + sal_tax_ppp['annual.federal.amount'] + sal_tax_ppp['annual.state.amount']
     sal_tax_ppp['Post Tax Annual Salary'] = (sal_tax_ppp['Annual Salary'] - sal_tax_ppp['Total Annual Tax'])
     sal_tax_ppp['Post Tax Annual Salary'] = sal_tax_ppp['Post Tax Annual Salary'].astype(int).apply(lambda x: floor(x / 100) * 100)
     sal_tax_ppp['Annual Salary Rounded'] = sal_tax_ppp['Annual Salary'].apply(lambda x: floor(x/100) / 10).astype(str).apply(lambda s: s+'k')
--- retrieve_data.py
+++ retrieve_data.py
@@ -1,4 +1,3 @@
-from clean_data import replace_dollar_with_float
 import scraper
 import data_io
 import engineer_features
@@ -6,24 +5,29 @@ import pandas as pd
 from debug_tools import *
 import api_calls
 from us_states_and_territories import states_only
+from clean_data import replace_dollar_with_float
 from math import floor
 
 # Note: this is now a dumb function. It does not determine whether
 # the query is good and return False if not. It assumes the query is
 # good and will error if not. Use check_job_search_term to check first.
-def get_salary_table_for_job_title(job_title: str, exemptions: int = 1, marital_status: str = 'single') -> pd.DataFrame:
+def get_salary_table_for_job_title(job_title: str) -> pd.DataFrame:
+    dprint('...in get_salary_table_for_job_title')
 
     # check if job is cached
-    job_cache_name = get_job_salary_file_name(job_title, exemptions, marital_status)
+    job_cache_name = get_job_salary_file_name(job_title)
     # if job is cached and recent enough, read from cache
     # TODO: implement cached file date checking. This should
     # always be true unless the cache is too old, because
     # the query should already be checked with check_job_search_term()
     if(data_io.file_exists(job_cache_name)):
+        dprint('salary already in cache')
         return data_io.read_df(job_cache_name)
     # else, scrape the job, save it in the cache, then return
     else:
         salary_df = replace_dollar_with_float(scraper.scrape_salary_table_for_job_title(job_title))
+        dprint('salary not in cache, scraped. salary_df after replacement:')
+        dprint(salary_df.head())
         data_io.write_df(salary_df, job_cache_name)
         return salary_df
 
@@ -33,13 +37,13 @@ def get_ppp_table() -> pd.DataFrame:
         # dprint('reading from file instead of net')
         return data_io.read_df(ppp_name)
     else:
-        ppp_table = scraper.scrape_ppp_table()
+        ppp_table = replace_dollar_with_float(scraper.scrape_ppp_table())
         data_io.write_df(ppp_table, ppp_name)
         return ppp_table
 
-def check_job_search_term(job_name: str, exemptions: int = 1, marital_status: str = 'single') -> bool:
+def check_job_search_term(job_name: str) -> bool:
     bad_query_list_file_name = "bad_queries.csv"
-    job_salary_data_file_name = get_job_salary_file_name(job_name, exemptions, marital_status)
+    job_salary_data_file_name = get_job_salary_file_name(job_name)
 
     # first check if file exists
     if(data_io.file_exists(job_salary_data_file_name)):
@@ -71,39 +75,43 @@ def check_job_search_term(job_name: str, exemptions: int = 1, marital_status: st
         data_io.write_list_to_csv(bad_queries, bad_query_list_file_name)
         return False
 
-def get_job_salary_file_name(job_name: str, exemptions: int = 1, marital_status: str = 'single'):
-    return f'salaries_{job_name}-{marital_status}-{exemptions}e.csv'
+def get_job_salary_file_name(job_name: str):
+    return f'salaries_{job_name}.csv'
 
 
-# unsure if this fully works!
+# TODO needs 
 def get_tax_all_states(salary_table: pd.DataFrame, marital_status: str = 'single', exemptions: int = 1) -> pd.DataFrame:
     tax_df = pd.DataFrame()
-    for state in salary_table['State Initial']:
-        dprint('getting tax info for :'+str(state))
-        tax_df.append(get_income_tax(state, salary_table['Annual Salary'][state], exemptions, marital_status))
+    counter = 0
+    for state in salary_table['State']:
+        if(counter < 5):
+            dprint('getting tax info for: '+state)
+            counter += 1
+        tax_df = tax_df.append(get_income_tax(state, salary_table.loc[salary_table['State']==state,'Annual Salary'].iloc[0], exemptions, marital_status))
     return tax_df
 
-floor_hundred = lambda x: floor(x/100)*100
+floor_hundred = lambda x: floor(int(x)/100)*100
 
-def get_income_tax(state_initial:str, annual_salary:int, exemptions:int = 1, marital_status:str = 'single', num_pay_periods: int = 1) -> int:
+def get_income_tax(state:str, annual_salary:int, exemptions:int = 1, marital_status:str = 'single', num_pay_periods: int = 1) -> int:
     # TODO read cache from file only once per session?
-    #########################################
-    # TODO - URGENT!! - add index by state initial as in other dataframes to allow for append above (in line 83) and merge in engineer features to work properly
+    
+    state_initial = states_only[state]
 
     # check if tax file exists and read it if so for adding to cache later else call api
     tax_cache_name = get_tax_cache_file_name()
     # annual.fica.amount  annual.federal.amount  annual.state.amount State Initial       State
     tax_cache_df = pd.DataFrame(columns=['annual.fica.amount','annual.federal.amount','annual.state.amount','State Initial','State']) 
     if(data_io.file_exists(tax_cache_name)):
-        # dprint('tax cache file exists, reading...')
+        dprint('tax cache file exists, reading...')
         # if file does exist, read it into tax_cache_df and check if it contains the data
         tax_cache_df = data_io.read_df(tax_cache_name)
-        # dprint('read into tax cache the following:')
-        # dprint(tax_cache_df.head())
+        dprint('read into tax cache the following:')
+        dprint(tax_cache_df.head())
         try: 
             s = f'trying to read ({state_initial}, {floor_hundred(annual_salary)}) from cache...'
             dprint(s)
-            row = tax_cache_df.loc[[(tax_cache_df['State Initial'] == state_initial) & (floor_hundred(tax_cache_df['Annual Salary']) == floor_hundred(annual_salary))]]
+            # create a dataframe from a row in the tax cache table where annual salary rounded to 100s and state initial are the same
+            row = pd.DataFrame(tax_cache_df.loc[(tax_cache_df['State Initial'] == state_initial) & (floor_hundred(tax_cache_df['Annual Salary']) == floor_hundred(annual_salary))])
             # exists in cache
             dprint('no error thrown, exists in cache')
             return row
@@ -112,14 +120,8 @@ def get_income_tax(state_initial:str, annual_salary:int, exemptions:int = 1, mar
             pass
     # file doesn't exist or data doesn't exist in cache, so call API, append to tax_cache_df, and write to file
 
-    # dprint('calling api_calls.get_yearly_income_tax_from_api on state initial: '+state_initial+', salary: '+str(annual_salary)+', exemptions: '+str(exemptions)+', marital status: '+marital_status)
     row = api_calls.get_yearly_income_tax_from_api(state_initial, annual_salary, exemptions, marital_status)
-    # dprint('read from API:')
-    # dprint(row.head())
     tax_cache_df = tax_cache_df.append(row)
-    # dprint('tax cache with new row appended:')
-    # dprint(tax_cache_df.head())
-    # dprint('writing df to file...')
     data_io.write_df(tax_cache_df, tax_cache_name)
     return tax_cache_df
 
