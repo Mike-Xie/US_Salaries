ef29d3a1dcf6fc8ae4354e6ecf1f36d5caee958f
mikewillems <mikewillems@user.noreply.github.com>
2021-04-12 13:21:27 -0700

    now fully working except for statement looking for tax data in cache (retrieve_data line 111) - it's not finding the data in the cache even when it's there and so reads the tax every time

--- retrieve_data.py
+++ retrieve_data.py
@@ -12,7 +12,6 @@ from math import floor
 # the query is good and return False if not. It assumes the query is
 # good and will error if not. Use check_job_search_term to check first.
 def get_salary_table_for_job_title(job_title: str) -> pd.DataFrame:
-    dprint('...in get_salary_table_for_job_title')
 
     # check if job is cached
     job_cache_name = get_job_salary_file_name(job_title)
@@ -21,25 +20,26 @@ def get_salary_table_for_job_title(job_title: str) -> pd.DataFrame:
     # always be true unless the cache is too old, because
     # the query should already be checked with check_job_search_term()
     if(data_io.file_exists(job_cache_name)):
-        dprint('salary already in cache')
         return data_io.read_df(job_cache_name)
     # else, scrape the job, save it in the cache, then return
     else:
-        salary_df = replace_dollar_with_float(scraper.scrape_salary_table_for_job_title(job_title))
-        dprint('salary not in cache, scraped. salary_df after replacement:')
-        dprint(salary_df.head())
+        salary_df = scape_and_clean_salary_table(job_title)
         data_io.write_df(salary_df, job_cache_name)
         return salary_df
 
 def get_ppp_table() -> pd.DataFrame:
     ppp_name = "ppp_table.csv"
     if(data_io.file_exists(ppp_name)): 
-        # dprint('reading from file instead of net')
         return data_io.read_df(ppp_name)
     else:
         ppp_table = replace_dollar_with_float(scraper.scrape_ppp_table())
+        dprint('replaced table:')
+        dprint(ppp_table.head())
         data_io.write_df(ppp_table, ppp_name)
         return ppp_table
+
+def scape_and_clean_salary_table(job_name):
+    return replace_dollar_with_float(scraper.scrape_salary_table_for_job_title(job_name))
 
 def check_job_search_term(job_name: str) -> bool:
     bad_query_list_file_name = "bad_queries.csv"
@@ -60,7 +60,7 @@ def check_job_search_term(job_name: str) -> bool:
     # dprint('job name not in bad query list, scraping to check')
 
     # attempt scraping to check whether query is scrapable
-    df = scraper.scrape_salary_table_for_job_title(job_name)
+    df = scape_and_clean_salary_table(job_name)
 
     # if so, return True after saving scraped data if not present already
     if(type(df) == pd.DataFrame):
@@ -69,9 +69,7 @@ def check_job_search_term(job_name: str) -> bool:
 
     # if scraping doesn't work, save in bad searches and return False
     else:
-        # dprint(f'bad_queries before: {bad_queries}')
         bad_queries.append(job_name)
-        # dprint(f'bad_queries after: {bad_queries}')
         data_io.write_list_to_csv(bad_queries, bad_query_list_file_name)
         return False
 
@@ -85,7 +83,7 @@ def get_tax_all_states(salary_table: pd.DataFrame, marital_status: str = 'single
     counter = 0
     for state in salary_table['State']:
         if(counter < 5):
-            dprint('getting tax info for: '+state)
+            # dprint('getting tax info for: '+state)
             counter += 1
         tax_df = tax_df.append(get_income_tax(state, salary_table.loc[salary_table['State']==state,'Annual Salary'].iloc[0], exemptions, marital_status))
     return tax_df
@@ -102,11 +100,10 @@ def get_income_tax(state:str, annual_salary:int, exemptions:int = 1, marital_sta
     # annual.fica.amount  annual.federal.amount  annual.state.amount State Initial       State
     tax_cache_df = pd.DataFrame(columns=['annual.fica.amount','annual.federal.amount','annual.state.amount','State Initial','State']) 
     if(data_io.file_exists(tax_cache_name)):
-        dprint('tax cache file exists, reading...')
         # if file does exist, read it into tax_cache_df and check if it contains the data
         tax_cache_df = data_io.read_df(tax_cache_name)
-        dprint('read into tax cache the following:')
-        dprint(tax_cache_df.head())
+        # dprint('read from tax cache file the following:')
+        # dprint(tax_cache_df.head())
         try: 
             s = f'trying to read ({state_initial}, {floor_hundred(annual_salary)}) from cache...'
             dprint(s)
@@ -116,7 +113,7 @@ def get_income_tax(state:str, annual_salary:int, exemptions:int = 1, marital_sta
             dprint('no error thrown, exists in cache')
             return row
         except KeyError:
-            dprint('error thrown, not in cache, reading from API instead...')
+            # dprint('error thrown, not in cache, reading from API instead...')
             pass
     # file doesn't exist or data doesn't exist in cache, so call API, append to tax_cache_df, and write to file
 
