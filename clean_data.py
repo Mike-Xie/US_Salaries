5b906fcc43ec6bc1f4de9110c6a6a272c6518445
Mike <yuhuanxie@gmail.com>
2021-04-14 21:07:58 -0700

    remove unused import in clean_data

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,5 @@
 import pandas as pd 
-from us_states_and_territories import states_and_territories
 from debug_tools import dprint
-
 
 """
     Pre-processing function to replace string values like "$32,443" with float 32443 so we can perform arithmetic on the dataframes in engineer_features
@@ -15,6 +13,6 @@ def replace_dollar_with_float(df: pd.DataFrame) -> pd.DataFrame:
 """
     Replaces empty dataframe cell values with 0 so we can perform arithmetic on the dataframes in engineer_features
 """
-def clean_income_tax_data(income_tax_data_all_states: pd.DataFrame) -> pd.DataFrame:
-    df = income_tax_data_all_states.fillna(0)
+def clean_income_tax_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.fillna(0)
     return df
